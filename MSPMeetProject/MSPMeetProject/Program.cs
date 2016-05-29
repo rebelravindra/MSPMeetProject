0c4c3a9cd2efa160c59b8dc77d61bc6f347e788a
Gautham Reddy <gauthamprince96@gmail.com>
2016-05-29 12:36:08 +0530

    Armstrong got added

--- MSPMeetProject/MSPMeetProject/Program.cs
+++ MSPMeetProject/MSPMeetProject/Program.cs
@@ -15,12 +15,33 @@ namespace MSPMeetProject
             Console.WriteLine("Addition is : " + (a + b));
            int x = multiplication(10, 20);
             Console.WriteLine("multiplication is : " + x);
+            Console.WriteLine(armstrong(371));
             Console.Read();
         }
 
         public static int multiplication(int a, int b)
         {
             return a * b;
+        }
+
+        public static bool armstrong(int x)
+        {
+            int temp = x;
+            int r, sum=0;
+            while (x>0)
+            {
+                r = x % 10;
+                sum = sum + r * r * r;
+                x = x / 10;
+            }
+            if (sum == temp)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
