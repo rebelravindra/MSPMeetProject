228df27fb798c66ef7a422131628d9d32b14b09e
Gautham Reddy <gauthamprince96@gmail.com>
2016-05-29 12:20:49 +0530

    Multiplication code added

--- MSPMeetProject/MSPMeetProject/Program.cs
+++ MSPMeetProject/MSPMeetProject/Program.cs
@@ -6,14 +6,21 @@ using System.Threading.Tasks;
 
 namespace MSPMeetProject
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
             int a = 10;
             int b = 20;
             Console.WriteLine("Addition is : " + (a + b));
+           int x = multiplication(10, 20);
+            Console.WriteLine("multiplication is : " + x);
             Console.Read();
+        }
+
+        public static int multiplication(int a, int b)
+        {
+            return a * b;
         }
     }
 }
